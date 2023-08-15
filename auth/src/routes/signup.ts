6562fc455dc31295046163ae9642850a8c4b5099
Vlad <kytsevolov@gmail.com>
2023-08-14 18:26:39 -0600

    user signup

---
+++ auth/src/routes/signup.ts
@@ -0,0 +1,34 @@
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import { User } from "../models/user";
+import { BadRequestError } from "../errors/bad-request-error";
+
+const router = express.Router();
+
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      throw new BadRequestError("Email in use");
+    }
+
+    const user = User.build({ email, password });
+
+    await user.save();
+
+    res.status(201).send(user);
+  }
+);
+
+export { router as signupRouter };
