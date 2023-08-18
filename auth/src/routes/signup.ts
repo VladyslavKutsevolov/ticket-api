e2f19a45b620524efb3f8930eee185737fe2e2b2
Vlad <kytsevolov@gmail.com>
2023-08-17 19:15:33 -0600

    added jwt

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import { User } from "../models/user";
 import { BadRequestError } from "../errors/bad-request-error";
+import jwt from "jsonwebtoken";
+import { validateRequest } from "../middleware/validate-request";
 
 const router = express.Router();
 
@@ -14,6 +16,7 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be between 4 and 20 characters"),
   ],
+  validateRequest,
   async (req: Request, res: Response) => {
     const { email, password } = req.body;
 
@@ -26,6 +29,15 @@ router.post(
     const user = User.build({ email, password });
 
     await user.save();
+
+    const userJwt = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_KEY! // ! means we are sure that the variable is defined
+    );
+
+    req.session = {
+      jwt: userJwt,
+    };
 
     res.status(201).send(user);
   }
