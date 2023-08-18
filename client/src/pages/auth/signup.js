0ff17c8a6dc2699454a31d5d4202dbb7ced8a7da
Vlad <kytsevolov@gmail.com>
2023-08-18 17:34:35 -0600

    setup client and sign up page

---
+++ client/src/pages/auth/signup.js
@@ -0,0 +1,138 @@
+import { useState } from "react";
+import useRequest from "../../hooks/use-request";
+import Router from "next/router";
+
+const Signup = () => {
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
+  const [fieldErrors, setFieldErrors] = useState(null);
+
+  const { doRequest } = useRequest({
+    url: "/api/users/signup",
+    method: "post",
+    body: credentials,
+    onFail: (err) => setFieldErrors(err.errors),
+    onSuccess: () => Router.push("/"),
+  });
+
+  const handleChange = (e) => {
+    setFieldErrors(
+      fieldErrors?.filter((error) => error.field !== e.target.name)
+    );
+
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    await doRequest();
+  };
+
+  console.log("fieldErrors", fieldErrors);
+
+  const fieldError = (field) => {
+    return fieldErrors?.find((e) => e.field === field)?.message;
+  };
+
+  return (
+    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img
+          className="mx-auto h-10 w-auto"
+          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+          alt="Your Company"
+        />
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Sign in to your account
+        </h2>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" onSubmit={handleSubmit}>
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                onChange={handleChange}
+                value={credentials.email}
+              />
+              {fieldError("email") && (
+                <p className="text-red-500 text-xs italic pt-2">
+                  {fieldError("email")}
+                </p>
+              )}
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Password
+              </label>
+              <div className="text-sm">
+                <a
+                  href="#"
+                  className="font-semibold text-indigo-600 hover:text-indigo-500"
+                >
+                  Forgot password?
+                </a>
+              </div>
+            </div>
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                onChange={handleChange}
+                value={credentials.password}
+              />
+              {fieldError("password") && (
+                <p className="text-red-500 text-xs italic pt-2">
+                  {fieldError("password")}
+                </p>
+              )}
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Sign in
+            </button>
+          </div>
+        </form>
+
+        <p className="mt-10 text-center text-sm text-gray-500">
+          Not a member?
+          <a
+            href="#"
+            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+          >
+            Start a 14 day free trial
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
---
+++ client/src/pages/index.js
@@ -0,0 +1,9 @@
+const Root = () => {
+  return (
+    <div>
+      <h1 className="text-red-800">React App!</h1>
+    </div>
+  );
+};
+
+export default Root;
