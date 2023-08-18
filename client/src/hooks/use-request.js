58fa28df7615294cad87d1d1bacb5585b0abe2e7
Vlad <kytsevolov@gmail.com>
2023-08-18 17:33:51 -0600

    use request hook

---
+++ client/src/hooks/use-request.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { useState } from "react";
+
+const useRequest = ({ url, method, body, onFail, onSuccess }) => {
+  const [errors, setErrors] = useState(null);
+
+  const doRequest = async (props = {}) => {
+    try {
+      setErrors(null);
+      const res = await axios[method](url, { ...body, ...props });
+
+      if (onSuccess) onSuccess(res.data);
+
+      return res.data;
+    } catch (e) {
+      console.log("FAIL", e);
+      if (e?.response && onFail) {
+        const error = e.response.data.errors;
+
+        if (error[0].message === "Email in use") {
+          return onFail({
+            errors: [{ field: "email", message: "Email in use" }],
+          });
+        }
+
+        onFail(e.response.data);
+
+        setErrors(e.response.data);
+      }
+    }
+  };
+
+  return { doRequest, errors, onFail };
+};
+
+export default useRequest;
