df2fc635b0475713fbcb5d25600c53c783785c3a
Vlad <kytsevolov@gmail.com>
2023-09-14 14:16:35 -0600

    added env for nats

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 import { app } from "./app";
 import { natsWrapper } from "./nats-wrapper";
-import { randomBytes } from "crypto";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -12,11 +11,23 @@ const start = async () => {
     throw new Error("MONGO_URI must be defined");
   }
 
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("NATS_CLUSTER_ID must be defined");
+  }
+
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS_URL must be defined");
+  }
+
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS_CLIENT_ID must be defined");
+  }
+
   try {
     await natsWrapper.connect(
-      "ticketing",
-      randomBytes(4).toString("hex"),
-      "http://nats-srv:4222"
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
     );
     natsWrapper.client.on("close", () => {
       console.log("NATS connection closed!");
