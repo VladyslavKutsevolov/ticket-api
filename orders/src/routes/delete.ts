695979d6d8bf1d9ad6fb45756fd821574125d4ae
Vlad <kytsevolov@gmail.com>
2023-09-15 17:31:52 -0600

    publish events

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -6,6 +6,8 @@ import {
   requireAuth,
 } from "@vticketing/common";
 import { Order } from "../models/order";
+import { OrderCancelledPublisher } from "../events/order-cancelled-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -26,6 +28,13 @@ router.delete(
     order.status = OrderStatus.Cancelled;
 
     await order.save();
+
+    new OrderCancelledPublisher(natsWrapper.client).publish({
+      id: order.id,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
 
     res.status(204).send(order);
   }
